fa00f1830572cc8d7483d137334454bcb92b78f7
remkeeper <rem@rem.asia>
2026-03-15 02:44:26 +0800

    fix:遵循ID自增

--- database/location.go
+++ database/location.go
@@ -5,12 +5,11 @@ import "github.com/syumai/workers/_templates/cloudflare/deploy-go/model"
 func InsertLocationData(record model.LocationRecord) error {
 	// 插入数据到 D1 数据库
 	query := `
-		INSERT INTO location_records (id, device_id,timestamp,latitude,longitude,altitude,signal_strength,speed,satellites,created_at)
+		INSERT INTO location_records (device_id,timestamp,latitude,longitude,altitude,signal_strength,speed,satellites,created_at)
 		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
 	`
 	_, err := locationDb.Exec(
 		query,
-		record.ID,
 		record.DeviceID,
 		record.Timestamp,
 		record.Latitude,
